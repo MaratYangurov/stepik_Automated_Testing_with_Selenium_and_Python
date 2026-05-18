2b57311621781606dbf0413a186f570a14d30f1f
MaratYangurov <marat.yangurov@gmail.com>
2026-05-18 18:09:18 +0500

    Коммит_12. Продолжаю главу "3.6"

--- 3_6_section/section_3_6_lesson_4.py
+++ 3_6_section/section_3_6_lesson_4.py
@@ -1,17 +1,36 @@
+import pytest
 import unittest
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 link = 'https://stepik.org/lesson/236895/step/1'
+class TestLogin:
+    def test_guest_should_see_login_link(self, browser, load_config):
+        browser.get(link)
 
-def test_guest_should_see_login_link(browser):
-    browser.get(link)
-    time.sleep(5)
-    button = browser.find_element(By.CLASS_NAME, 'ember-view navbar__auth navbar__auth_login st-link st-link_style_button')
-    button.click()
+        # Нажать кнопку "Войти"
+        time.sleep(10)
+        login_button = browser.find_element(By.CSS_SELECTOR, '.navbar__auth_login')
+        login_button.click()
 
+        # Ждём появления формы логина
+        wait = WebDriverWait(browser, 10)
+        username_input = wait.until(EC.presence_of_element_located((By.NAME, 'login')))
+        password_input = browser.find_element(By.NAME, 'password')
+        login = load_config['login_stepik']
+        password = load_config['password_stepik']
+        username_input.send_keys(login)
+        password_input.send_keys(password)
 
+        # Нажимаем кнопку "Войти"
+        browser.find_element(By.CLASS_NAME, 'sign-form__btn').click()
 
+        # Ждём, пока попап авторизации исчезнет
+        wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, ".modal--opened")))
+
+        # Проверяем, что авторизация успешна
+        assert "lesson" in browser.current_url
--- conftest.py
+++ conftest.py
@@ -3,11 +3,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 
-@pytest.fixture(scope="function")
+
+@pytest.fixture(scope='session')
 def browser():
     # Фикстура для браузера
-    print("\nstart browser for test..")
+    print('\nstart browser for test..')
     browser = webdriver.Chrome()
     yield browser
-    print("\nquit browser..")
+    print('\nquit browser..')
     browser.quit()
+
+
+@pytest.fixture(scope='session')
+def load_config():
+    # Открываем файл с конфигом в режиме чтения
+    with open('config.json', 'r') as config_file:
+        # С помощью библиотеки json читаем и возвращаем результат
+        config = json.load(config_file)
+        return config
