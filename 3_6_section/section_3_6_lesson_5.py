66aba1f4e45fa83a1ce1ba4ea9898e60a6ba46e9
Marat_Yangurov <marat.yangurov@gmail.com>
2026-05-26 21:41:23 +0500

    Коммит_14. Продолжаю главу "3.6"

--- 3_6_section/section_3_6_lesson_5.py
+++ 3_6_section/section_3_6_lesson_5.py
@@ -2,16 +2,16 @@ import pytest
 import math
 import time
 
+from selenium.common import StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestParametrize:
-
     @staticmethod
     def calc_answer():
-        """Вычисление актуального ответа"""
+        # Вычисление актуального ответа
         return str(math.log(int(time.time())))
 
     @staticmethod
@@ -21,30 +21,15 @@ class TestParametrize:
         if browser.find_elements(By.CSS_SELECTOR, '.navbar__profile-img'):
             return
         # Кнопка "Войти"
-        login_button = wait.until(
-            EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, '.navbar__auth_login')
-            )
-        )
+        login_button = wait.until(EC.element_to_be_clickable((By.ID, 'ember503')))
         login_button.click()
         # Поле логина
-        username_input = wait.until(
-            EC.visibility_of_element_located(
-                (By.NAME, 'login')
-            )
-        )
+        username_input = wait.until(EC.visibility_of_element_located((By.ID, 'id_login_email')))
         # Поле пароля
-        password_input = browser.find_element(
-            By.NAME,
-            'password'
-        )
+        password_input = browser.find_element(By.ID, 'id_login_password')
         # Ввод данных
-        username_input.send_keys(
-            load_config['login_stepik']
-        )
-        password_input.send_keys(
-            load_config['password_stepik']
-        )
+        username_input.send_keys(load_config['login_stepik'])
+        password_input.send_keys(load_config['password_stepik'])
         # Кнопка входа
         submit_login = browser.find_element(
             By.CSS_SELECTOR,
@@ -52,60 +37,47 @@ class TestParametrize:
         )
         submit_login.click()
         # Ждём закрытия модального окна
-        wait.until(
-            EC.invisibility_of_element_located(
-                (By.CSS_SELECTOR, '.modal--opened')
-            )
-        )
+        wait.until(EC.invisibility_of_element_located((By.CSS_SELECTOR, '.modal--opened')))
 
     @pytest.mark.parametrize(
         'link',
         [
             'https://stepik.org/lesson/236895/step/1',
             'https://stepik.org/lesson/236896/step/1',
-            'https://stepik.org/lesson/236897/step/1',
-            'https://stepik.org/lesson/236898/step/1',
-            'https://stepik.org/lesson/236899/step/1',
-            'https://stepik.org/lesson/236903/step/1',
-            'https://stepik.org/lesson/236904/step/1',
-            'https://stepik.org/lesson/236905/step/1',
+            # 'https://stepik.org/lesson/236897/step/1',
+            # 'https://stepik.org/lesson/236898/step/1',
+            # 'https://stepik.org/lesson/236899/step/1',
+            # 'https://stepik.org/lesson/236903/step/1',
+            # 'https://stepik.org/lesson/236904/step/1',
+            # 'https://stepik.org/lesson/236905/step/1',
         ]
     )
-    def test_parametrize_section_3_6_lesson_5(
-            self,
-            browser,
-            load_config,
-            link
-    ):
+    def test_parametrize_section_3_6_lesson_5(self, browser, load_config, link):
         wait = WebDriverWait(browser, 20)
         # Открываем страницу
         browser.get(link)
         # Авторизация
         self.login_stepik(browser, load_config)
         # Поле ответа
-        textarea = wait.until(
-            EC.element_to_be_clickable(
-                (By.TAG_NAME, 'textarea')
-            )
-        )
-        # Очистка поля
-        textarea.clear()
+        textarea = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'textarea')))
         # Ввод актуального ответа
-        textarea.send_keys(
-            self.calc_answer()
-        )
-        # Кнопка "Отправить"
-        submit_button = wait.until(
-            EC.element_to_be_clickable(
-                (By.CSS_SELECTOR, '.submit-submission')
-            )
-        )
-        time.sleep(3)
-        submit_button.click()
+        textarea.send_keys(self.calc_answer())
+        # Ждем пока все обновиться
+        time.sleep(2)
+        # Пытаемся нажать кнопку 'Отправить' несколько раз
+        for i in range(10):
+            try:
+                submit_button = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.submit-submission')))
+
+                submit_button.click()
+                break
+            except StaleElementReferenceException:
+                time.sleep(1)
+
         # Получение feedback
         feedback = wait.until(
             EC.visibility_of_element_located(
-                (By.CSS_SELECTOR, '.smart-hints__hint')
+                (By.CLASS_NAME, 'show-more__content')
             )
         )
         feedback_text = feedback.text
