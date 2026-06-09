b74985597ec1412c0f325c71012b3bf3386246ac
MaratYangurov <marat.yangurov@gmail.com>
2026-06-09 18:28:20 +0500

    Коммит_18. Продолжаю главу "3.6", задание test_section_3_6_lesson_10.py

--- conftest.py
+++ conftest.py
@@ -1,22 +1,38 @@
 import pytest
 from selenium import webdriver
 import json
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
+    # выбираем каким браузером открыть тест Chrome или Firefox
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
+    # выбираем на каком языке проводить тест, по умолчанию eng
+    parser.addoption('--language', action='store', default='en',
+                     help='Choose language')
 
 @pytest.fixture(scope='function')
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption('language')
     browser = None
+
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.set_preference(
+            'intl.accept_languages',
+            user_language
+        )
+
+        browser = webdriver.Firefox(options=firefox_options)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
