3b4b4acd7b5034e58bf5ae198d1cd6d9d83d4a2f
MaratYangurov <marat.yangurov@gmail.com>
2026-06-10 18:03:52 +0500

    Коммит_19. Продолжаю главу "3.6", задание test_section_3_6_lesson_10.py

--- test_3_6_section/test_items_section_3_6_lesson_10.py
+++ test_3_6_section/test_items_section_3_6_lesson_10.py
@@ -7,11 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from conftest import browser, load_config
 from pages.stepik_login_page import LoginPage
 
-class TestParametrize:
+class TestDiffLang:
     @staticmethod
-    def setup(cls):
-        pass
+    @pytest.fixture(scope='function')
+    def setup(browser, load_config):
+        # Открываем страницу
+        browser.get(f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
+        browser.implicitly_wait(10)
 
     def test_login_page(self, setup):
-        pass
+        print(f'Говно работает!')
 
